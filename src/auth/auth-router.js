e684d0cf387caa909b4d0d047261be4c562b98c9
Kathy Bradt <kathybradt717@gmail.com>
2019-12-11 21:42:40 -0600

    added user_id to response from login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -37,6 +37,7 @@ authRouter
               const payload = { user_id: dbUser.user_id }
               res.send({
                   authToken: AuthService.createJwt(sub, payload),
+                  payload,
               })
               
           })
